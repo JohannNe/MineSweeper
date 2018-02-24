ff944d216fe5b2ffa4eb3fe3ece4c275d4ae0fc2
JohannNe <36745242+JohannNe@users.noreply.github.com>
2018-02-25 00:13:48 +0100

    Counting mines implemented
    Moved parsing fields into an ArrayList into a seperate function.
    Implemented a function to count how many mines touch a square.

--- MineSweeperKata/src/MineSweeper.java
+++ MineSweeperKata/src/MineSweeper.java
@@ -12,12 +12,42 @@ public class MineSweeper {
   
   public static String[][] convertFields(String[][] fieldsInput){
     String[][] fieldsOutput = null;
+    ArrayList<String[][]> arrFields = parseInputToArrayList(fieldsInput);
+    
+    
+    //get in the numbers for mines
+    
+    
+    
+    return fieldsOutput;
+  }
+  
+  //Check how many mines are touching the square in line "iLine" and column "iColumn" of given field "field"
+  public static int giveNumberOfMines(String[][] field, int iLine, int iColumn){
+    int numberOfMines=0;
+    
+    for(int i = iLine-1; i<=iLine+1; i++){
+      for(int j=iColumn-1; j<= iColumn+1;j++){
+        //check if the indexes do not go out of bounds
+        if(i!=-1 && j!=-1 && i!=field.length && j!=field[0].length){ 
+          if(field[i][j]=="*"){
+            //There is a mine touching this field
+            numberOfMines+=1;
+          }
+        }
+      }      
+    }
+    
+    return numberOfMines;
+  }
+  
+  //Parse the given input of fields into seperate 2-dimensional arrays and add them to a ArrayList that gets returned.
+  public static void parseInputToArrayList(String[][] fieldsInput){
     ArrayList<String[][]> arrFields = new ArrayList<String[][]>();
     String[][] newField = null;
     int iFieldLines;
     int iFieldColums;
     int index=0;
-    
     
     while (fieldsInput[index][0] != 0+""){
       //Parse one field and add to ArrayList
@@ -34,11 +64,13 @@ public class MineSweeper {
       printFields(newField); //Testing if my parsing of fields works.
       arrFields.add(newField);
       
+      System.out.println(giveNumberOfMines(newField, 1,1)); //Testing giveNumberOfMinesImplementation for index (1,1)
       index += iFieldLines+1;
     }
     
-    return fieldsOutput;
+    return arrFields;
   }
+  
   
   //function for console output of two dimensional string arrays.
   public static void printFields(String[][] fields){
