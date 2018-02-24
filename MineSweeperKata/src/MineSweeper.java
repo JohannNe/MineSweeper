2742367e1e40e79ba701eeb0236e6912b4d6fe2e
JohannNe <36745242+JohannNe@users.noreply.github.com>
2018-02-24 22:33:53 +0100

    Basic structure created
    Console output of example fields created.

--- MineSweeperKata/src/MineSweeper.java
+++ MineSweeperKata/src/MineSweeper.java
@@ -1,4 +1,22 @@
 
 public class MineSweeper {
-
+  
+  public static void main(String[]args){
+    String [][] fields = {{"4","4"},{"*",".",".",".",},{".",".",".",".",},{".","*",".",".",},{".",".",".",".",} //
+                          {"3","5"},{"*","*",".",".","."},{".",".",".",".","."},{".","*",".",".","."},{"0","0"}};
+    //Test console output of inputFields
+    for(int i = 0; i<fields.length; i++){
+      for(int j=0; j<fields.length; j++){
+        System.out.print(fields[i][j]);
+      }
+      System.out.println();
+    }
+    
+  }
+  
+  public static String[][] convertFields(String[][] fieldsInput){
+    String[][] fieldsOutput;
+    
+    return fieldsOutput;
+  }
 }
